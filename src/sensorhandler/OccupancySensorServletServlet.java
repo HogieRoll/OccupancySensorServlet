afdf2e4f31d1d189878d51f623f1b275d25dcd37
Alex Hoganson <alexh.458@gmail.com>
2016-02-11 12:06:07 -0600

    Changed Datastore Value

--- src/sensorhandler/OccupancySensorServletServlet.java
+++ src/sensorhandler/OccupancySensorServletServlet.java
@@ -1,7 +1,15 @@
 package sensorhandler;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.http.*;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 @SuppressWarnings("serial")
 public class OccupancySensorServletServlet extends HttpServlet {
@@ -11,6 +19,16 @@ public class OccupancySensorServletServlet extends HttpServlet {
 		resp.getWriter().println("Hello, from Occusensor Online!\n"+
 		                         "******************************\n"+
 								 "Database update successful");
+	}
+	public void doPut(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		String guestbookName = req.getParameter("guestbookName");
+        
+ 
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put("Hello");
+ 
+        resp.sendRedirect("/guestbook.jsp?guestbookName=" + guestbookName);
 	}
 	
 }
