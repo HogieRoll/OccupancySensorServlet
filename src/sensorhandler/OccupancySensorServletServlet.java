2f0368e0829fd8e20f594f6794b77d1f4d189b07
Alex Hoganson <alexh.458@gmail.com>
2016-02-11 11:27:42 -0600

    First One!

---
+++ src/sensorhandler/OccupancySensorServletServlet.java
@@ -0,0 +1,16 @@
+package sensorhandler;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class OccupancySensorServletServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, from Occusensor Online!\n"+
+		                         "******************************\n"+
+								 "Database update successful");
+	}
+	
+}
